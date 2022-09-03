d775d771ca61177f2f359902adb14dafc76e014c
yves123k <78654829+yves123k@users.noreply.github.com>
2022-09-03 16:35:36 +0000

    Add files via upload

---
+++ fontion_1.py
@@ -0,0 +1,8 @@
+import math
+def calculator(ab,bc):
+
+    calcule= int(ab)*int(ab)+int(bc)*int(bc)
+    print(math.sqrt(calcule)) 
+    
+
+calculator(ab=input('entre ab'),bc=input('entre bc'))    
